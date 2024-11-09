401cdfd1ddd6644be0c331c833e004a35be34013
umesh <umesh98904@gmail.com>
2024-11-09 10:19:19 +0530

    Login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -304,7 +304,7 @@ export const verifyLoginOtp = async (req, res) => {
   try {
     // Find user by email
     const user = await User.findOne({ email });
-
+    const user_id = user._id;
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
@@ -333,6 +333,7 @@ export const verifyLoginOtp = async (req, res) => {
 
     res.status(200).json({
       message: "Login successful",
+      user_id,
       accessToken,
       refreshToken,
     });
