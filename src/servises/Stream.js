a10b0b71c939eec958ac414c57234a92f3f6ffb5
umesh <umesh98904@gmail.com>
2024-12-02 17:57:23 +0530

    online update

--- src/servises/Stream.js
+++ src/servises/Stream.js
@@ -1,30 +1,37 @@
-import User from "../models/Users";
-
-
+import mongoose from "mongoose";
+import User from "../models/Users.js";
 
 export const watchUserChanges = () => {
-  const changeStream = User.watch();
+  try {
+    const changeStream = User.watch();
 
-  changeStream.on("change", async (change) => {
-    if (change.operationType === "update") {
-      const updatedFields = change.updateDescription.updatedFields;
+    changeStream.on("change", async (change) => {
+      console.log("Change event detected:", change);
 
-      if (updatedFields.userType === "RECEIVER") {
-        try {
-          const user = await User.findById(change.documentKey._id);
-          if (user) {
-            user.status = "Online";
-            await user.save();
-            console.log(`User ${user._id} is now Online (RECEIVER).`);
+      if (change.operationType === "update") {
+        const updatedFields = change.updateDescription.updatedFields;
+
+        if (updatedFields.userType === "RECEIVER") {
+          try {
+            const user = await User.findById(change.documentKey._id);
+            if (user) {
+              user.status = "Online";
+              await user.save();
+              console.log(`User ${user._id} is now Online (RECEIVER).`);
+            }
+          } catch (error) {
+            console.error("Error updating user status:", error);
           }
-        } catch (error) {
-          console.error("Error updating user status:", error);
         }
       }
-    }
-  });
+    });
 
-  changeStream.on("error", (error) => {
-    console.error("Change stream error:", error);
-  });
+    changeStream.on("error", (error) => {
+      console.error("Change stream error:", error);
+    });
+
+    console.log("Change stream initialized.");
+  } catch (error) {
+    console.error("Error initializing watchUserChanges:", error);
+  }
 };
