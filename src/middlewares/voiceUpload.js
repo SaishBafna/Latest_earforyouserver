66aa32f4f43f01bfaaa59f039f72ee991d5ec5da
tejas kahirnar <tejaskhairnar256@gmail.com>
2025-12-25 13:55:31 +0530

    save

--- src/middlewares/voiceUpload.js
+++ src/middlewares/voiceUpload.js
@@ -1,23 +1,20 @@
-import multer from 'multer';
+import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from '../config/cloudinary.js';
-
+import cloudinary from "../config/cloudinary.js";
 
 const voiceStorage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "user_voice_records",
-    resource_type: "video", // 🔴 REQUIRED for audio
-    allowed_formats: ["mp3", "wav", "webm", "m4a"],
-    public_id: (req, file) => `voice_${Date.now()}`,
+    resource_type: "video", // REQUIRED for audio
+    allowed_formats: ["mp3", "wav", "webm", "m4a","mp4"],
+    public_id: (req) => `voice_${req.params.userId}_${Date.now()}`,
   },
 });
 
 const uploadVoice = multer({
   storage: voiceStorage,
-  limits: {
-    fileSize: 10 * 1024 * 1024, // 10MB limit
-  },
+  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB
 });
 
 export default uploadVoice;
